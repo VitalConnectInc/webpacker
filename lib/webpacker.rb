4bf3577dcdac9ae6aaf87380a0732f4182cb6aa9
Tarun Chaudhry <tarunc@users.noreply.github.com>
2024-11-23 15:15:08 -0800

    Update webpacker.rb

--- lib/webpacker.rb
+++ lib/webpacker.rb
@@ -1,7 +1,8 @@
-require "active_support/core_ext/module/attribute_accessors"
-require "active_support/core_ext/string/inquiry"
-require "active_support/logger"
-require "active_support/tagged_logging"
+# TaggedLogging is an auto-loaded constant.
+# require "active_support/core_ext/module/attribute_accessors"
+# require "active_support/core_ext/string/inquiry"
+# require "active_support/logger"
+# require "active_support/tagged_logging"
 
 module Webpacker
   extend self
